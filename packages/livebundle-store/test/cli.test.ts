0af654d97607b44b82586362c0a629916b43bf5b
Benoit Lemaire <lemaireb@gmail.com>
2020-06-25 13:41:46 -0700

    timeout

--- packages/livebundle-store/test/cli.test.ts
+++ packages/livebundle-store/test/cli.test.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import "mocha";
 import sinon from "sinon";
 
-describe("index", () => {
+describe("cli", () => {
   it("should not fail importing cli", () => {
     const argv = process.argv;
     process.argv = ["node", "livebundle-github", "--help"];
