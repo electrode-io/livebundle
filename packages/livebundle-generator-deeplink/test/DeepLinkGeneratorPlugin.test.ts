4d9a77403b391271bc87c740026e37ec78756c32
Benoit Lemaire <lemaireb@gmail.com>
2020-11-25 09:16:50 -0800

    Add host to DeepLink generator config

--- packages/livebundle-generator-deeplink/test/DeepLinkGeneratorPlugin.test.ts
+++ packages/livebundle-generator-deeplink/test/DeepLinkGeneratorPlugin.test.ts
@@ -7,30 +7,30 @@ import { LiveBundleContentType } from "livebundle-sdk";
 describe("DeepLinkGeneratorPlugin", () => {
   describe("create", () => {
     it("should return an instance of DeepLinkGeneratorPlugin", async () => {
-      const res = await DeepLinkGeneratorPlugin.create();
+      const res = await DeepLinkGeneratorPlugin.create({host: "io.livebundle"});
       expect(res).instanceOf(DeepLinkGeneratorPlugin);
     });
   });
 
   describe("generate", () => {
     it("should generate a LiveBundle package DeepLink", async () => {
-      const sut = new DeepLinkGeneratorPlugin();
+      const sut = new DeepLinkGeneratorPlugin({host: "io.livebundle"});
       const id = uuidv4();
       const result = await sut.generate({
         id,
         type: LiveBundleContentType.PACKAGE,
       });
-      expect(result.deepLinkUrl).eql(`livebundle://packages?id=${id}`);
+      expect(result.deepLinkUrl).eql(`livebundle://io.livebundle/packages?id=${id}`);
     });
 
     it("should generate a LiveBundle session DeepLink", async () => {
-      const sut = new DeepLinkGeneratorPlugin();
+      const sut = new DeepLinkGeneratorPlugin({host: "io.livebundle"});
       const id = uuidv4();
       const result = await sut.generate({
         id,
         type: LiveBundleContentType.SESSION,
       });
-      expect(result.deepLinkUrl).eql(`livebundle://sessions?id=${id}`);
+      expect(result.deepLinkUrl).eql(`livebundle://io.livebundle/sessions?id=${id}`);
     });
   });
 });
